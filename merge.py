2a9a26f00593025036dbf58dafbc167db178cef8
xxx595588 <daniel0323.lin@gmail.com>
2022-11-11 14:11:36 -0800

    new test file added

--- merge.py
+++ merge.py
@@ -20,6 +20,7 @@ def merge():
     file_reader = []
 
     # remove for mac only, windows can ignore
+    files_to_read.remove(".DS_Store")
     files_to_read = sorted(files_to_read)
 
     # store the current posting of each file
@@ -28,7 +29,8 @@ def merge():
     # create reader, one per file
     for index_file in files_to_read:
         file_reader.append(open(index_file, "r"))
-
+    
+    # import postings into cur_posting
     for i in range(len(file_reader)):
         data = file_reader[i].readline()
         if data == "":
@@ -36,4 +38,59 @@ def merge():
         else:
             cur_posting.append(index_converter(data))
             
-    # do some merge algo here...
+    # word list which is corresopnding to cur_posting
+    word_list = list()
+    
+    for post in cur_posting:
+        if post == "eof":
+            word_list.append("~")
+        else:
+            word_list.append(post.get_word())
+    
+    while True:
+        # obtain the nmuber of posting needs to be mergerd
+        num_to_merge = word_list.count(min(word_list))
+        
+        # only one minimum posting was found, no need to merge
+        if num_to_merge == 1:
+            index = word_list.index(min(word_list))
+            to_be_merged = cur_posting[index]
+            
+            # read the next word for file_reader[index]
+            data = file_reader[index].readline()
+            
+            # check if reach end of file
+            if data == "":
+                cur_posting[index] = "eof"
+                word_list[index] = "~"
+            else:
+                cur_posting[index] = index_converter(data)
+                word_list[index] = cur_posting[index].get_word()
+                
+            # write the signle posting to the disk...
+            # (write code here)
+            
+        else:
+            # indicate which cur_posting are about to be merged as a list of indexs
+            indexs = [i for i in range(len(word_list)) if word_list[i] == min(word_list)]
+            to_be_merged = list()
+            
+            for i in indexs:
+                # gather all postings
+                to_be_merged.append(cur_posting[i])
+                data = file_reader[i].readline()
+                
+                # check if reach end of file
+                if data == "":
+                    cur_posting[i] = "eof"
+                    word_list[i] = "~"
+                else:
+                    cur_posting[i] = index_converter(data)
+                    word_list[i] = cur_posting[i].get_word()
+                    
+            # merge the to_be_merged here...
+            # (write code here)
+        
+        # check if reach end of file for all files
+        if cur_posting.count("eof") == len(files_to_read):
+            break
