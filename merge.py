fa8eb8aa2c921089d7e78255310fc9d6c5ef571c
xxx595588 <daniel0323.lin@gmail.com>
2022-11-11 01:40:17 -0800

    merge function start code

---
+++ merge.py
@@ -0,0 +1,37 @@
+import os
+import posting
+
+def index_converter(raw_data):
+    # convert raw_data into word: posting
+    word = raw_data.split("-> ")[0]
+    raw_posting = raw_data.split("-> ")[1][:-1]
+
+    id_freq = eval(raw_posting.split(", ID/pos: ")[0][9:])
+    id_pos = eval(raw_posting.split(", ID/pos: ")[1])
+
+    return posting.posting(word, id_freq, id_pos)
+
+
+def merge():
+    path = "index files"
+    os.chdir(path)
+
+    # list of index file to be merged
+    files_to_read = os.listdir()
+    file_reader = []
+
+    # remove for mac only, windows can ignore
+    files_to_read.remove(".DS_Store")
+    files_to_read = sorted(files_to_read)
+
+    # store the current posting of each file
+    cur_posting = []
+    
+    # create reader, one per file
+    for index_file in files_to_read:
+        file_reader.append(open(index_file, "r"))
+
+    for i in range(len(file_reader)):
+        cur_posting.append(index_converter(file_reader[i].readline()))
+
+    # do some merge algo here...
