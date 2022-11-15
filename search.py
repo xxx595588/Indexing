d5e7c580a85a72af6df91bfd336811c155f5e68c
xxx595588 <daniel0323.lin@gmail.com>
2022-11-15 13:51:33 -0800

    applied binary search in search.py

--- search.py
+++ search.py
@@ -1,4 +1,11 @@
 import json
+import time
+import linecache
+from posting import posting
+from nltk import ngrams
+
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 # find the intersection of given list
 def find_intersection(allPostings):
@@ -12,45 +19,78 @@ def find_intersection(allPostings):
 
     return intersec
 
+def binary_search(f, start, end, word, indexer_list, allPostings):
+
+    # valid conditioin
+    if end >= start:
+        mid = (start + end) // 2
+
+        line = linecache.getline("merged_indexer.txt", mid)
+        loaded = json.loads(line)
+
+        # compare with mid
+        if word == loaded["token"]:
+            postings = posting(word, eval(loaded["postings"]), eval(loaded["positions"]))
+            indexer_list.append(postings)
+            allPostings.append(postings.get_freq().keys())
+        # search start to mid - 1
+        elif word < loaded["token"]:
+            binary_search(f, start, mid - 1, word, indexer_list, allPostings)
+        # search mid + 1 to end
+        else:
+            binary_search(f, mid + 1, end, word, indexer_list, allPostings)
+        
+    
 def search():
     query = input("Enter your query seperated by spaces: ")
     queries = query.split(" ")
+
+    # doing ngram
+    ngram_iteration = [2]
+    ngram_temp = list()
+
+    for iter in ngram_iteration:
+        ngramTokens = list(ngrams(queries, iter))
+        for ngram in ngramTokens:
+            ngram_temp.append(ngram)
+
+    queries += ngram_temp
+
+    for i in range(len(queries)):
+        if(type(queries[i]) == tuple):
+            queries[i] = " ".join(list(queries[i]))
+
+
+    f = open("indicator.txt", "r")
+    indicator = eval(f.readline()[:-1])
 
     # list of query words' docID, use for finding intersection
     allPostings = list()
 
     # dictionary of id/freq, will be used in tf-idf calculation
-    id_freq_list = list()
+    indexer_list = list()
 
     # sort the query for better performance
     queries = sorted(queries)
+    global alphabet
+    
+    start = time.time()
 
-    f = open("indexer2.txt", "r")
-
-    # only search for the word in query
     for word in queries:
-        line = f.readline()
-        loaded = json.loads(line)
-        
-        # find the first matched word with queries
-        while loaded["token"] != word:
-            line = f.readline()
+        start_pos = indicator[alphabet.index(word[0])]
+        end_pos = indicator[alphabet.index(word[0]) + 1] - 1
+        f = open("merged_indexer.txt", "r")
+        binary_search(f, start_pos, end_pos, word, indexer_list, allPostings)
+        f.close()
+ 
+    for i in indexer_list:
+        print(f"{i.get_word()}: id/freq is {i.get_freq()}")
 
-            if line == "":
-                break
+    if len(allPostings) != 1:
+        intersec = find_intersection(allPostings)
+    else:
+        intersec = list(allPostings[0])
 
-            loaded = json.loads(line)
+    end = time.time()
 
-        if line == "":
-            break
-        
-        # store the info
-        postings = eval(loaded["postings"])
-        id_freq_list.append(postings)
-        allPostings.append(postings.keys())
-
-    f.close()
-
-    intersec = find_intersection(allPostings)
-
-    # calculate the score...
+    print(f"time is: {end-start} sec")
