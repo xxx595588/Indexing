9da97782a03b0156a4798ca730db58da33efc772
TeeCakes <44686305+TeeCakes@users.noreply.github.com>
2022-11-30 16:05:34 -0800

    Added spacing between elements

--- gui.py
+++ gui.py
@@ -22,19 +22,24 @@ inputtxt = tk.Text(frame,
                    height = 1,
                    width = 50)
 
-inputtxt.pack()
+inputtxt.pack(side = tk.BOTTOM)
+
+inputtxt.place(x = 100, y = 10)
 
 # Search Button
 printButton = tk.Button(frame,
                         text = "Search",
                         command = printResults)
-printButton.pack()
+printButton.pack(side = tk.BOTTOM)
+
+printButton.place(x = 275, y = 40)
 
 frame.bind('<Return>', handler)
 
 
 # Results Label
 lbl = tk.Label(frame, text = "", wraplength=500)
-lbl.pack()
+lbl.pack(side = tk.BOTTOM, ipady = 30)
+
 frame.resizable(False,False) # prevent resizing of window
 frame.mainloop()
